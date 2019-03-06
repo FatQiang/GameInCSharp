fecb490ef97f2335f020dd54c0748e6291397f2f
zhongyingqiang <934291938@qq.com>
2019-03-06 16:30:06 +0800

    Create LevelClass.cs

---
+++ Sokoban/Sokoban/LevelClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokoban
+{
+    public class LevelClass
+    {
+        public int Num = -1;
+        public string Name = "";
+        public int[] ManLocation = new int[] { -1, -1 };
+        public List<int[]> BoxList = null;
+        public int[,] Map = null;
+    }
+}
